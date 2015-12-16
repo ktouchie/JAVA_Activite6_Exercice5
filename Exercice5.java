e2fb8ddd00a13ceb307ddf7d95e2eb37f0004d1a
Karin Touchie <ktouchie@gmail.com>
2015-12-16 10:33:55 +0100

    Duplicate method from Act2 Ex7 and integrate with DD/MM/YYYY methods

--- Exercice5.java
+++ Exercice5.java
@@ -2,6 +2,8 @@ import java.util.*;
 class Exercice5 {
 
 	public static void main(String[] args) {
+        
+        System.out.println("Veuillez saisir un jour:");
        
 	}
 
@@ -21,11 +23,92 @@ class Exercice5 {
         if (a>=-10000) {
             return true;
         }
-        
     }
     
     public static boolean date(int day, int month, int year) {
-        
+        if (jour(day)) {
+            if (mois(month)) {
+                if (annee(year)) {
+                    if (jour>30) {
+                        if (mois<8) {
+                            if (mois%2==1) {
+                                valid = true;
+                            } else {
+                                valid = false;
+                            }
+                        } else {
+                            if (mois%2==1) {
+                                valid = false;
+                            } else {
+                                valid = true;
+                            }
+                        }
+                    } else if (jour==29) {
+                        if (mois==2) {
+                            if (annee%100==0) {
+                                if (annee%400==0) {
+                                    valid = true;
+                                } else {
+                                    valid = false;
+                                }
+                            } else {
+                                if (annee%4==0) {
+                                    valid = true;
+                                } else {
+                                    valid = false;
+                                }
+                            }
+                        } else {
+                            valid = true;
+                        }
+                    } else {
+                        valid = true;
+                    }
+                }
+            }
+        }
         
     }
 }
+
+
+
+
+
+if (jour>31) {
+            valid = false;
+        } else if (jour>30) {
+            if (mois<8) {
+                if (mois%2==1) {
+                    valid = true;
+                } else {
+                    valid = false;
+                }
+            } else {
+                if (mois%2==1) {
+                    valid = false;
+                } else {
+                    valid = true;
+                }
+            }
+        } else if (jour==29) {
+            if (mois==2) {
+                if (annee%100==0) {
+                    if (annee%400==0) {
+                        valid = true;
+                    } else {
+                        valid = false;
+                    }
+                } else {
+                    if (annee%4==0) {
+                        valid = true;
+                    } else {
+                        valid = false;
+                    }
+                }
+            } else {
+                valid = true;
+            }
+        } else {
+            valid = true;
+        }
